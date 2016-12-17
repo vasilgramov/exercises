d36d14d6012f9224c2013ad0797bc69edbe4f255
Vladix97 <gramovv@gmail.com>
2016-12-17 15:31:46 +0200

    added two algorithmic challenges

---
+++ challenges/easy/hr_funnyString.java
@@ -0,0 +1,37 @@
+package hackerRank.challenges.easy;
+
+import java.util.Scanner;
+
+public class hr_funnyString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        for (int test = 0; test < testCases; test++) {
+            String inputText = in.nextLine();
+            String reversedInput = new StringBuilder(inputText).reverse().toString();
+
+            boolean isFunny = true;
+
+            outerloop:
+            for (int i = 1; i < inputText.length(); i++) {
+                int firstResult = Math.abs(inputText.charAt(i) - inputText.charAt(i - 1));
+                for (int j = i; j <= i; j++) {
+                    int secondResult = Math.abs(reversedInput.charAt(j) - reversedInput.charAt(i - 1));
+
+                    if (firstResult != secondResult) {
+                        isFunny = false;
+                        break outerloop;
+                    }
+                }
+            }
+
+            if (isFunny) {
+                System.out.println("Funny");
+            }
+            else {
+                System.out.println("Not Funny");
+            }
+        }
+    }
+}
---
+++ challenges/easy/hr_sherlockAndTheBeast.java
@@ -0,0 +1,54 @@
+package hackerRank.challenges.easy;
+
+import java.util.Scanner;
+
+public class hr_sherlockAndTheBeast {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int testsCount = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < testsCount; i++) {
+            int currentNumber = Integer.parseInt(in.nextLine());
+
+            if (currentNumber < 3) {
+                System.out.println(-1);
+                continue;
+            }
+            else if (currentNumber % 3 == 0) {
+                System.out.println(newString("555", currentNumber / 3));
+                continue;
+            }
+
+            int count = 1;
+            boolean hasFound = false;
+            while (count <= currentNumber) {
+                if ((currentNumber - count) % 3 == 0 && count % 5 == 0) {
+                    System.out.println(newString("555", (currentNumber - count) / 3) + newString("33333", count / 5));
+                    hasFound = true;
+                    break;
+                }
+
+                count++;
+            }
+
+            if (!hasFound) {
+                if (currentNumber % 5  == 0) {
+                    System.out.println(newString("33333", count / 5));
+                }
+                else {
+                    System.out.println(-1);
+                }
+            }
+        }
+    }
+
+    private static String newString(String str, int count) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            builder.append(str);
+        }
+
+        return builder.toString();
+    }
+}
