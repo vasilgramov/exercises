85d1e8632486d7183a6be0b519a61a9d2730efa8
Vladix97 <gramovv@gmail.com>
2016-12-30 22:02:34 +0200

    optimized two strings challenge

--- challenges/easy/hr_twoStrings.java
+++ challenges/easy/hr_twoStrings.java
@@ -1,7 +1,7 @@
 // https://www.hackerrank.com/challenges/two-strings?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=3-day-campaign
 package hackerRank.challenges.easy;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class hr_twoStrings {
     public static void main(String[] args) {
@@ -10,29 +10,22 @@ public class hr_twoStrings {
         int numberOfTestCases = Integer.parseInt(in.nextLine());
 
         for (int i = 0; i < numberOfTestCases; i++) {
-            String firstStr = in.nextLine();
-            String secondStr = in.nextLine();
-
+            String firstInput = in.nextLine();
+            String secondInput = in.nextLine();
             boolean hasFound = false;
-            if (firstStr.length() <= secondStr.length()) {
-                for (int j = 0; j < firstStr.length(); j++) {
-                    if (secondStr.indexOf(firstStr.charAt(j)) != -1) {
-                        System.out.println("YES");
-                        hasFound = true;
-                        break;
-                    }
-                }
-            } else {
-                for (int j = 0; j < secondStr.length(); j++) {
-                    if (firstStr.indexOf(secondStr.charAt(j)) != -1) {
-                        System.out.println("YES");
-                        hasFound = true;
-                        break;
-                    }
+
+            for (int j = 97; j < 122; j++) {
+                char currentChar = (char)j;
+
+                if (firstInput.indexOf(currentChar) != -1 && secondInput.indexOf(currentChar) != -1) {
+                    hasFound = true;
+                    break;
                 }
             }
-
-            if (!hasFound) {
+            
+            if (hasFound) {
+                System.out.println("YES");
+            } else {
                 System.out.println("NO");
             }
         }
