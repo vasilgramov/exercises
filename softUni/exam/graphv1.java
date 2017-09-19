17b8dbaa304b8891965de02fcbbd4ce20706f699
Vladix97 <gramovv@gmail.com>
2017-09-19 11:51:18 +0300

    softuni graph problem

--- softUni/exam/graphv1.java
+++ softUni/exam/graphv1.java
@@ -1,48 +1,98 @@
-package hackerRank.challenges;
+package softUni.exam;
 
 import java.util.*;
 
-public class hr_evenTree {
+public class graphv1 {
 
     private static Set<Integer> visited = new HashSet<>();
     private static Map<Integer, Node> graph = new HashMap<>();
 
+    private static int max = Integer.MIN_VALUE;
+    private static int sum = 0;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-
         int vertices = in.nextInt();
         int connections = in.nextInt();
+        int startingPoint = in.nextInt();
 
+        readGraph(in, connections);
+
+        Node root = graph.get(startingPoint);
+        traverseGraph(root);
+
+        visited = new HashSet<>();
+        List<Edge> edges = getRemovedEdges();
+
+        splitGraph(edges);
+
+        visited = new HashSet<>();
+        getMaxComponentSum();
+
+        System.out.println(max);
+    }
+
+    private static void getMaxComponentSum() {
+        for (Map.Entry<Integer, Node> kvp : graph.entrySet()) {
+
+            sum = 0;
+            dfs(kvp.getKey());
+
+            if (sum > max) max = sum;
+        }
+    }
+
+    private static void readGraph(Scanner in, int connections) {
         for (int i = 0; i < connections; i++) {
             int u = in.nextInt();
             int v = in.nextInt();
 
-            updateMap(graph, u, v);
-            updateMap(graph, v, u);
+            updateGraph(graph, u, v);
+            updateGraph(graph, v, u);
         }
+    }
 
+    private static void splitGraph(List<Edge> edges) {
+        for (Edge edge : edges) {
+            graph.get(edge.u).connections.remove((Object) edge.v);
+            graph.get(edge.v).connections.remove((Object) edge.u);
+        }
+    }
 
-        Node root = graph.get(1);
-        traverseGraph(root);
-
-        visited = new HashSet<>();
-        int count = 0;
+    private static List<Edge> getRemovedEdges() {
+        List<Edge> edges = new ArrayList<>();
         for (Node node : graph.values()) {
             visited.add(node.index);
             List<Integer> children = node.connections;
+
             for (Integer child : children) {
                 Node childNode = graph.get(child);
 
                 if (childNode.numberOfChildren % 2 == 0 && !visited.contains(childNode.index)) {
                     node.numberOfChildren -= childNode.numberOfChildren;
+
+                    edges.add(new Edge(childNode.index, node.index));
+
                     visited.add(childNode.index);
-                    count++;
                 }
             }
         }
+        return edges;
+    }
 
-        System.out.println(count);
+    private static void dfs(int idx) {
+        if (visited.contains(idx)) return;
+
+        visited.add(idx);
+
+        Node node = graph.get(idx);
+        sum += node.index;
+
+        List<Integer> connections = node.connections;
+        for (Integer c : connections) {
+            dfs(c);
+        }
     }
 
     private static void traverseGraph(Node node) {
@@ -65,7 +115,7 @@ public class hr_evenTree {
         node.numberOfChildren++;
     }
 
-    private static void updateMap(Map<Integer, Node> nodeByIndex, int u, int v) {
+    private static void updateGraph(Map<Integer, Node> nodeByIndex, int u, int v) {
         if (!nodeByIndex.containsKey(u)) {
             Node node = new Node();
             node.index = u;
@@ -77,7 +127,22 @@ public class hr_evenTree {
         }
     }
 
-    public static class Node {
+    private static class Edge {
+        private int u;
+        private int v;
+
+        public Edge(int u, int v) {
+            this.u = u;
+            this.v = v;
+        }
+
+        @Override
+        public String toString() {
+            return this.u + " " + this.v;
+        }
+    }
+
+    private static class Node {
         private int index;
         private List<Integer> connections;
         private int numberOfChildren;
