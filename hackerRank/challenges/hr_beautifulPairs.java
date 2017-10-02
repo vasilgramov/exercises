a74dbd88f28d88e936a9532d6e46fdfd59a2511c
Vladix97 <gramovv@gmail.com>
2017-10-02 11:04:44 +0300

    modified // addded comments

--- hackerRank/challenges/hr_beautifulPairs.java
+++ hackerRank/challenges/hr_beautifulPairs.java
@@ -1,5 +1,7 @@
 package hackerRank.challenges;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -9,81 +11,61 @@ public class hr_beautifulPairs {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-//
-//        int n = in.nextInt();
-//        int[] a = new int[n];
-//        int[] b = new int[n];
-//
-//        fillArr(in, a);
-//        fillArr(in, b);
-//
-//        Map<Integer, Integer> memoA = new TreeMap<>();
-//        Map<Integer, Integer> memoB = new TreeMap<>();
-//
-//        for (int i = 0; i < n; i++) {
-//            int numA = a[i];
-//            int numB = b[i];
-//
-//            updateMemo(memoA, numA);
-//            updateMemo(memoB, numB);
-//        }
-//
-//        int pairs = 0;
-//        boolean hasUsed = false;
-//        for (Map.Entry<Integer, Integer> kvp : memoA.entrySet()) {
-//            int key = kvp.getKey();
-//            int val = kvp.getValue();
-//
-//            Integer count = memoB.get(key);
-//            if (count != null) {
-//                if (val > count && !hasUsed) {
-//                    pairs += count + 1;
-//                    hasUsed = true;
-//                } else {
-//                    pairs += Math.min(val, count);
-//                }
-//            }
 
         int n = in.nextInt();
-        int a[] = new int[1001];
-        int sum = 0;
-        for(int i=1;i<=n;i++)
-            a[in.nextInt()]++;
+        int[] a = new int[n];
+        int[] b = new int[n];
 
+        fillArr(in, a);
+        fillArr(in, b);
 
-        for(int i=1;i<=n;i++)
-        {
-            int v = in.nextInt();
-            if(a[v]>0)
-            {
-                sum++;
-                a[v]--;
-            }
+        Map<Integer, Integer> memoA = new HashMap<>();
+        Map<Integer, Integer> memoB = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int numA = a[i];
+            int numB = b[i];
+
+            updateMemo(memoA, numA);
+            updateMemo(memoB, numB);
         }
 
 
+        int counter = getPairwiseDisjointBeautifulPairs(memoA, memoB);
 
-        if(sum<n)
-            System.out.print(sum+1);
-        else
-            System.out.print(sum-1);
+        if (counter + 1 > n) {  // if the arrays are equal we must change one element and we get n - 1 pairs
+            System.out.println(counter - 1);
+        } else {                // we have the count of equal elements, before changing the element
+            System.out.println(counter + 1);
+        }
     }
 
-//        System.out.println(pairs);
+    private static int getPairwiseDisjointBeautifulPairs(Map<Integer, Integer> memoA, Map<Integer, Integer> memoB) {
+        int counter = 0;
+        for (Map.Entry<Integer, Integer> kvp : memoA.entrySet()) {
+            int key = kvp.getKey();
+            int valA = kvp.getValue();
 
+            Integer valB = memoB.get(key);
+            if (valB != null) {
+                counter += Math.min(valA, valB);
+            }
+        }
+        return counter;
+    }
 
-//    private static void updateMemo(Map<Integer, Integer> memo, int n) {
-//        if (!memo.containsKey(n)) {
-//            memo.put(n, 1);
-//        } else {
-//            memo.put(n, memo.get(n) + 1);
-//        }
-//    }
-//
-//    private static void fillArr(Scanner in, int[] arr) {
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] = in.nextInt();
-//        }
-//    }
+    private static void updateMemo(Map<Integer, Integer> memo, int n) {
+        if (!memo.containsKey(n)) {
+            memo.put(n, 1);
+        } else {
+            memo.put(n, memo.get(n) + 1);
+        }
+    }
+
+    private static void fillArr(Scanner in, int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+    }
 }
